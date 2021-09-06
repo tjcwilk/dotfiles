9a0a5150ad8e970d87a55e34ddd52165634c2c4c
Toby W <toby@wilkins.io>
2021-09-06 15:57:15 +0100

    add symlink for tmux sesshions

--- symlink.sh
+++ symlink.sh
@@ -13,3 +13,9 @@ ln -sf $DIR/.tmux.conf ~/.tmux.conf
 
 echo "-> home folder shortcuts"
 ln -sf $DIR/cheatsheet.md ~/cheatsheet.md
+
+echo "-> tmux new shortcut"
+ln -sf $DIR/tmux-new.sh ~/tmux-new.sh
+
+echo "-> tmux attach shortcut"
+ln -sf $DIR/tmux-attach.sh ~/tmux-attach.sh
