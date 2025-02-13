be62bf002375d85176b591e697e37c8829a53995
Ubuntu <toby.w@null>
2025-02-13 13:42:57 +0000

    .

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ sudo apt-get install -y vim git tree tmux
 # configure git 
 echo "Configuring git"
 git config --global user.name "Toby W"
-git config --global user.email "toby@wilkins.io"
+git config --global user.email "toby@null"
 git config --global core.editor "vim"
 
 echo "Sourcing files"
