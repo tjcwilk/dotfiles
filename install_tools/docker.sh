008d228c0a738a881ac2c66aedf7551548753279
Toby W <toby@properbutton.com>
2026-04-20 10:26:23 +0100

    tidyup

--- install_tools/docker.sh
+++
@@ -1,7 +0,0 @@
-# -----------------------------------------------------------------------------
-# Install Docker
-# -----------------------------------------------------------------------------
-echo "========== Installing Docker =========="
-sudo apt install -y docker.io docker-compose
-sudo systemctl enable docker --now
-sudo usermod -aG docker "$USER"
